b2bf3a993611967753b783d000ae25a91723d77e
Orcun Atakan <oatakan@gmail.com>
2016-08-15 12:42:22 -0500

    create test database for the docker php app

--- docker-multi-node/php-mysql-app/code/db.php
+++ docker-multi-node/php-mysql-app/code/db.php
@@ -21,7 +21,7 @@
 if(!$_ENV["VCAP_SERVICES"]){ //local dev
     $mysql_server_name = "mysql:3306";
     $mysql_username = "root";
-    $mysql_password = "passw0rd";
+    $mysql_password = "";
     $mysql_database = "test";
 } else { //running in Bluemix
     $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
